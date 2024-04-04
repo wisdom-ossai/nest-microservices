ca39e7d382e14587df3132497bb8c03ee5495446
Wisdom Ossai <ossaiwisdom48@gmail.com>
2024-04-05 00:07:17 +0100

    implement abstract repository to handle app-wide crud

--- libs/common/src/database/abstract.respository.ts
+++ libs/common/src/database/abstract.respository.ts
@@ -1,6 +1,6 @@
 import { Logger, NotFoundException } from '@nestjs/common';
 import { AbstractDocument } from './abstract.schema';
-import { FilterQuery, Model, Types } from 'mongoose';
+import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
 
 /**
  * @Repository
@@ -35,5 +35,37 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
     }
 
     return document;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = await this.model
+      .findOneAndUpdate(filterQuery, update, {
+        new: true,
+      })
+      .lean<TDocument>(true);
+
+    if (!document) {
+      this.logger.warn(
+        'Document was not found with the pass query = ',
+        filterQuery,
+      );
+
+      throw new NotFoundException('Document was not found!');
+    }
+
+    return document;
+  }
+
+  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
+    return await this.model.find(filterQuery).lean<TDocument[]>(true);
+  }
+
+  async findOneAndDelete(
+    filterQuery: FilterQuery<TDocument>,
+  ): Promise<TDocument> {
+    return await this.model.findOneAndDelete(filterQuery).lean<TDocument>(true);
   }
 }
