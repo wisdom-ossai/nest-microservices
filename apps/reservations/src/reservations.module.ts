948f09ad8f85799c6a3e7882aed638159edaa746
Wisdom Ossai <ossaiwisdom48@gmail.com>
2024-04-10 03:44:33 +0100

    create CRUD for reservations

---
+++ apps/reservations/src/reservations.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ReservationsController } from './reservations.controller';
+import { ReservationsService } from './reservations.service';
+import { DatabaseModule } from '@app/common';
+import { ReservationsRepository } from './reservations.repository';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './schema/reservation.schema';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema },
+    ]),
+  ],
+  controllers: [ReservationsController],
+  providers: [ReservationsService, ReservationsRepository],
+})
+export class ReservationsModule {}
---
+++ apps/reservations/src/reservations.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { CreateReservationDto } from './dto/create-reservation.dto';
+import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { ReservationsRepository } from './reservations.repository';
+
+@Injectable()
+export class ReservationsService {
+  constructor(
+    private readonly reservationsRepository: ReservationsRepository,
+  ) {}
+  create(createReservationDto: CreateReservationDto) {
+    return this.reservationsRepository.create({
+      ...createReservationDto,
+      userId: '123',
+      timestamp: new Date(),
+    });
+  }
+
+  findAll() {
+    return this.reservationsRepository.find({});
+  }
+
+  findOne(_id: string) {
+    return this.reservationsRepository.findOne({
+      _id,
+    });
+  }
+
+  update(_id: string, updateReservationDto: UpdateReservationDto) {
+    return this.reservationsRepository.findOneAndUpdate(
+      {
+        _id,
+      },
+      {
+        $set: updateReservationDto,
+      },
+    );
+  }
+
+  remove(_id: string) {
+    return this.reservationsRepository.findOneAndDelete({
+      _id,
+    });
+  }
+}
